359a04728387c7040222a6bb13993238d593f3cc
ella(엘라/강송희) <ella.hee@kakaostyle.com>
2023-07-05 01:21:04 +0900

    결제페이지

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,12 +6,13 @@ import {EXECUTE_PAY} from "../graphql/payment";
 const mock_products = (() => Array.from({length: 20}).map(
     (_, i) => ({
       id: i + 1 + '',
-      imageUrl: `https://placeimg.com/200/150/${i + 1}`,
+      imageUrl: `https://picsum.photos/id/${i + 10}/200/150`,
       price: 50000,
       title: `임시상품${i + 1}`,
       description: `임시상세내용${i + 1}`,
-      createdAt: new Date(1683819024912 + (i * 1000 * 60 * 60 * 10)).toString(),
-    })))()
+      createdAt: new Date(1683819024912 + (i * 1000 * 60 * 60 * 10)).toString()
+    })
+))()
 
 let cartData: { [key: string]: CartType } = (() => ({}))()
 
@@ -35,31 +36,31 @@ export const handlers = [
     return res(ctx.data(cartData))
   }),
   graphql.mutation(ADD_CART, (req, res, ctx) => {
-    const newCartData = { ...cartData }
+    const newCartData = {...cartData }
     const id = req.variables.id
+
     const targetProduct = mock_products.find(item => item.id === req.variables.id)
     if (!targetProduct) { throw new Error('상품이 없습니다') }
 
     const newItem = {
       ...targetProduct,
-      amount: (newCartData[id]?.amount || 0) + 1
+      amount: (newCartData[id]?.amount || 0) + 1,
+    }
+    newCartData[id] = newItem
+    cartData = newCartData
+    return res(ctx.data(newItem))
+  }),
+  graphql.mutation(UPDATE_CART, (req, res, ctx) => {
+    const newCartData = {...cartData }
+    const { id, amount } = req.variables
+    if (!newCartData[id]) { throw new Error('없는 데이터입니다.') }
+    const newItem = {
+      ...newCartData[id],
+      amount,
     }
     newCartData[id] = newItem
     cartData = newCartData
 
-    return res(ctx.data(newItem))
-  }),
-  graphql.mutation(UPDATE_CART, (req, res, ctx) => {
-    const newData = { ...cartData }
-    const { id, amount } = req.variables
-
-    if (!newData[id]) { throw new Error('없는 데이터입니다') }
-    const newItem = {
-      ...newData[id],
-      amount,
-    }
-    newData[id] = newItem
-    cartData = newData
     return res(ctx.data(newItem))
   }),
   graphql.mutation(DELETE_CART, ({variables: {id}}, res, ctx) => {
@@ -68,7 +69,10 @@ export const handlers = [
     cartData = newData
     return res(ctx.data({id}))
   }),
-  graphql.mutation(EXECUTE_PAY, ({ variables} , res, ctx) => {
-    return res()
+  graphql.mutation(EXECUTE_PAY, ({ variables: ids} , res, ctx) => {
+    ids.forEach((id: string)  => {
+      delete cartData[id]
+    })
+    return res(ctx.data(ids))
   }),
 ]
---
+++ src/recoils/cart.ts
@@ -0,0 +1,27 @@
+import {atom, selectorFamily, useRecoilValue} from "recoil";
+import {CartType} from "../graphql/cart";
+
+export const checkedCartState = atom<CartType[]>(
+    {
+        key: 'cartState',
+        default: []
+    }
+)
+
+// export const cartItemSelector = selectorFamily<number | undefined, string>({
+//         key: 'cartItem',
+//         get: (id: string) => ({get}) => {
+//             const carts = get(cartState)
+//             return carts.get(id)
+//         },
+//         set: (id: string) => ({get, set}, newValue) => {
+//             if(typeof newValue === 'number') {
+//
+//                 const newCart = new Map([...get(cartState)])
+//                 newCart.set(id, newValue)
+//                 set(cartState, newCart)
+//             }
+//         }
+//
+//     }
+// )
